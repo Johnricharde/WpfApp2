8807e522bb00d47681c628f1042fe595842d5fec
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-03-12 13:12:53 +0100

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void runButtonToggle_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                textBlockTest.Text = "Test running";
+                runButtonToggle.Content = "RUN";
+            }
+            else
+            {
+                textBlockTest.Text = "Test stopped";
+                runButtonToggle.Content = "STOP";
+            }
+            running = !running;
+        }
+    }
+}
